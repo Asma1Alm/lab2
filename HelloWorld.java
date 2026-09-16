d47bf66d8127268789f42132561983d92a732811
Asma <445052086@std.psau.edu.sa>
2026-09-16 10:01:17 +0300

    s

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! Welcome to Git.");
         System.out.println("new update");
+        System.out.println("new update2");
+
 
     }
 }
